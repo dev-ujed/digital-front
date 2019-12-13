e3ca9dd88b18c7270039abe968ceb1c9f9a0abcc
Xochitl <xochitl.escamilla@ujed.mx>
2019-12-13 16:04:37 -0600

    Agregar django-inline-svg

--- digitalproject/settings.py
+++ digitalproject/settings.py
@@ -47,6 +47,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'digitalapp',
+    'svg'
 ]
 
 MIDDLEWARE = [
@@ -129,3 +130,7 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 STATICFILES_DIRS = (os.path.join(BASE_DIR, 'static'),)
+
+SVG_DIRS=[
+    os.path.join(BASE_DIR, 'static/img/icons')
+]
