39621292305fb5cb4e287c77e8d460401a13c941
Ramón Arturo Morales <arturo.morales@ujed.mx>
2019-12-18 14:15:15 -0600

    Conectar stepform con views.py para guardad información a la base de datos.

--- digitalapp/views.py
+++ digitalapp/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render
 from django.http import HttpResponse
-import requests
+import requests, json
 from requests.auth import HTTPBasicAuth
 
 
@@ -11,19 +11,21 @@ def index(request):
 
 
 def save(request):
+    data_request = json.loads(request.body.decode('utf-8'))
+
     data = {
-        "matricula":"123465",
-        "ures": 1,
-        "nombre": "ejemplo",
-        "apellido_paterno": "ejem",
-        "apellido_materno": "plos",
-        "correo": "ejemplo@ujed.mx",
-        "extension": "542",
-        "telefono": "12345678",
-        "pautoriza": "EjemploMayor",
+        "matricula": data_request['matricula'],
+        "ures": data_request['unit'],
+        "nombre": data_request['name'],
+        "apellido_paterno": data_request['first_last_name'],
+        "apellido_materno": data_request['second_last_name'],
+        "correo": data_request['email'],
+        "extension": data_request['extension'],
+        "telefono": data_request['phone'],
+        "pautoriza": data_request['from'],
         "servicio": 2,
         "subservicio": 1,
-        "descripcion": "Deseo un sitio web de ejemplo",
+        "descripcion": data_request['request_description'],
         "descripcon_archivo": "",
     }
 
