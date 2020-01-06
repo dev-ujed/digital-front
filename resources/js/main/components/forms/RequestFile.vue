2777bb6fe01ad0dd8633f83ead8928fea38190c5
Xochitl <xochitl.escamilla@ujed.mx>
2020-01-06 10:35:31 -0600

    Crear componente para subir archivos

---
+++ resources/js/main/components/forms/RequestFile.vue
@@ -0,0 +1,44 @@
+<template>
+    <div>
+        <div class="card mb-4" 
+        v-for="(file, key) in files" 
+            :key="key" 
+            :value="file.name">
+            <div class="card__title">
+                <p class="m-0 d-initial" >
+                <slot paperclip="paperclip" name="paperclip" ></slot>
+                    <i> {{ file.name }} </i>
+                </p>
+            
+                <button class="btn--without-style color-danger p-0 card__button">
+                    <slot name="x"></slot>
+                </button>
+            </div>
+            <div class="card__body card--file">
+                <div class="card card--thumb mr-8 m-auto sm:d-block text-center">
+                    <img src="" alt="">
+                </div>
+                <div class="form-control m-0">
+                    <label :for="'description_'+key" class="size-caption">Descripción del archivo <small class="color-gray-60"><i>Opcional</i></small></label>
+                    <input type="text" :id="'description_'+key" :name="'description_'+key" class="form-field">
+                    <small class="color-gray-60">Máximo 80 caracteres</small>
+                </div>
+            </div>
+        </div>
+    </div>
+</template>
+
+<script>
+    export default {
+        props: {
+            files: {
+                type: Array,
+                required: true
+            },
+        },
+
+        mounted() {
+
+        }
+    }
+</script>
---
+++ resources/js/main/components/forms/RequestUploadFile.vue
@@ -0,0 +1,49 @@
+<template>
+    <div>
+        <p class="size-caption" v-if="files.length > 0">Archivos adjuntos</p>
+        <p class="size-sm color-gray-60" v-if="files.length > 0">Si lo deseas, puedes agregar una breve anotación a los archivos que has agregado.</p>
+
+        <request-file
+            :files="files"
+            v-if="files.length > 0"
+        >
+
+            <slot name="paperclip" slot="paperclip"></slot>
+            <slot name="x" slot="x"></slot>
+           
+        </request-file>
+
+        <div class="form-control mb-18">
+            <label for="file" class="btn btn--sm btn--light cursor--pointer">
+                Adjuntar archivo
+            </label>
+            <input class="form-field hidden" id="file" name="file" type="file" @change="addFile">
+        </div>
+
+    </div>
+</template>
+
+<script>
+    import RequestFile from './RequestFile.vue';
+
+    export default {
+
+        components: { RequestFile },
+        data() {
+            return {
+                files: [],
+            };
+        },
+        methods: {
+            addFile() {
+
+                this.hasFiles = true;
+                  
+                this.files.push(event.target.files[0]);     
+            }
+        },
+        mounted() {
+            
+        }
+    }
+</script>
