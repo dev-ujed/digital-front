d026627aa5eb8479099338be38ed9e588a528b19
Xochitl <xochitl.escamilla@ujed.mx>
2020-01-28 13:02:48 -0600

    Remover cambios de merge

--- digitalproject/settings.py
+++ digitalproject/settings.py
@@ -32,9 +32,9 @@ environ.Env.read_env(env_file=os.path.join(BASE_DIR, '.env'))
 SECRET_KEY = env.str('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = env.bool('DEBUG')
 
-ALLOWED_HOSTS =  ['127.0.0.1']
+ALLOWED_HOSTS = []
 
 
 # Application definition
