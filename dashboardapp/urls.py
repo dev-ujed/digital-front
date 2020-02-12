1ec3666b874b3f301489dbc46deffaad025f25ed
Xochitl <xochitl.escamilla@ujed.mx>
2020-02-12 15:39:08 -0600

    Agregar URL de solicitudes

--- dashboardapp/urls.py
+++ dashboardapp/urls.py
@@ -5,5 +5,8 @@ from . import views
 urlpatterns = [
     path('', views.index, name='index'),
     path('solicitudes/bandeja', views.inbox, name='inbox'),
+    path('solicitudes', views.solicitudes, name='solicitudes'),
+    path('solicitud', views.solicitud, name='solicitud'),
+
 ]
 
