bbabf683415676b9e4781398b76544095fa7ac60
Ramón Arturo Morales <arturo.morales@ujed.mx>
2020-02-12 16:17:21 -0600

    Agregar validación para no saltarse login.

--- dashboardapp/urls.py
+++ dashboardapp/urls.py
@@ -5,7 +5,6 @@ from . import views
 urlpatterns = [
     path('', views.index, name='index'),
     path('solicitudes/bandeja', views.inbox, name='inbox'),
-    path('solicitudes', views.solicitudes, name='solicitudes'),
     path('solicitud', views.solicitud, name='solicitud'),
 
 ]
