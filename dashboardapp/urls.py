89734af8bc8d388b86e993263c52ef7bc8b5da26
Ramón Arturo Morales <arturo.morales@ujed.mx>
2020-02-12 14:31:41 -0600

    Ajustar ruta para bandeja de solicitudes.

--- dashboardapp/urls.py
+++ dashboardapp/urls.py
@@ -4,6 +4,6 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name='index'),
-    path('solicitudes', views.solicitudes, name='solicitudes'),
+    path('solicitudes/bandeja', views.inbox, name='inbox'),
 ]
 
