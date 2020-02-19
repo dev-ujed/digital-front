7d53f05936aa52b3eadd9782fbef3ccef24c546b
Arturo Morales <arturo.morales@ujed.mx>
2020-02-19 12:41:38 -0600

    Consultar solicitudes desde la base de datos en bandeja de solicitudes.

--- dashboardapp/views/solicitudes.py
+++ dashboardapp/views/solicitudes.py
@@ -1,8 +1,12 @@
 from django.shortcuts import render, redirect
 from django.http import HttpResponse, JsonResponse
 import requests
+from datetime import datetime
+from django.utils.dateparse import parse_date
+import locale
 
 def inbox(request):
+    locale.setlocale(locale.LC_TIME, '')
     if request.session.is_empty():
         domain = request.build_absolute_uri('/')[:-1]
         return redirect(domain+'/digitalapp/ingresar')
@@ -163,15 +167,25 @@ def inbox(request):
         status   = requests.get('http://192.168.10.46:8000/solicitudes/sol/cat_estatus/')
         statuses = {}
 
+        requestByStatus = {}
+
         for state in status.json():
             statuses.update({ state['key_name']: state['Descripcion']})
+            response = requests.get('http://192.168.10.46:8000/solicitudes/sol/estatus_sol/'+str(state['id']))
+            requestByStatus.update({ state['key_name']: response.json() })
 
-        # response    = requests.get('http://192.168.10.46:8000/solicitudes/sol/solicitudes/all/')
-        # solicitudes = response.json()
 
-        # return HttpResponse(solicitudes)
+        for key, value in requestByStatus.items():
+            for element in value:
+                date = datetime.strptime(element['fecha_sol'], "%d/%m/%Y %H:%M")
+                formatedDate = date.strftime("%d %h %Y, %I:%M %p")
+                element.update({'formated_date' : formatedDate})
+                archivos = eval(element['archivos'][0])
+                element.update({'archivos': archivos['count']})
+
 
         return render(request, "solicitudes/index.html", {
             'statuses': statuses,
-            'dataUrl': dataUrl
+            'dataUrl': dataUrl,
+            'requestByStatus': requestByStatus
         })
