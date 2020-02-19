38a195987ff8d4141e55c30e789e09a24687ef1f
Arturo Morales <arturo.morales@ujed.mx>
2020-02-19 14:21:41 -0600

    Agregar servicios a solicitudes.

--- dashboardapp/views/solicitudes.py
+++ dashboardapp/views/solicitudes.py
@@ -7,6 +7,7 @@ import locale
 
 def inbox(request):
     locale.setlocale(locale.LC_TIME, '')
+    services= []
     if request.session.is_empty():
         domain = request.build_absolute_uri('/')[:-1]
         return redirect(domain+'/digitalapp/ingresar')
@@ -171,18 +172,25 @@ def inbox(request):
 
         for state in status.json():
             statuses.update({ state['key_name']: state['Descripcion']})
-            response = requests.get('http://192.168.10.46:8000/solicitudes/sol/estatus_sol/'+str(state['id']))
+            response = requests.get('http://192.168.10.46:8000/solicitudes/sol/estatus_sol/' + str(state['id']))
             requestByStatus.update({ state['key_name']: response.json() })
 
 
         for key, value in requestByStatus.items():
             for element in value:
+
                 date = datetime.strptime(element['fecha_sol'], "%d/%m/%Y %H:%M")
                 formatedDate = date.strftime("%d %h %Y, %I:%M %p")
                 element.update({'formated_date' : formatedDate})
+
                 archivos = eval(element['archivos'][0])
                 element.update({'archivos': archivos['count']})
 
+                if element['servicios'] != None:
+                    for servicio in element['servicios']:
+                        services.append(eval(servicio))
+
+                    element.update({'servicios': services})
 
         return render(request, "solicitudes/index.html", {
             'statuses': statuses,
