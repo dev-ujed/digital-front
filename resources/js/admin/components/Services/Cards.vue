53eb8aad874b781701d50433164c1fa24b3b3ae9
Xochitl <xochitl.escamilla@ujed.mx>
2020-02-24 15:25:22 -0600

    Agregar componente de servicios

---
+++ resources/js/admin/components/Services/Cards.vue
@@ -0,0 +1,28 @@
+<template>
+    <div>
+        <div class="text-center mt-6">
+            <button class="btn btn--light w-full" @click="showModal">
+                <span class="icon-plus-circle">
+                    <slot name="plus-circle"></slot>
+                </span>
+                Crear servicio
+            </button>
+        </div>
+    </div>
+</template>
+<script>
+
+    import ServiceModal from './ServiceModal';
+
+    export default {
+        components: { ServiceModal },
+        methods: {
+            showModal() {
+                this.$root.$emit('showModal');
+            },
+        },
+        mounted() {
+            console.log('Servicios');
+        },
+    };
+</script>
