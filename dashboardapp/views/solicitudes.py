ec04d556f274f50f4922b7a97f5e6ebdd9be7681
Arturo Morales <arturo.morales@ujed.mx>
2020-02-25 14:55:58 -0600

    Ajustar view de solicitudes para ajustar  contenido de la api.

--- dashboardapp/views/solicitudes.py
+++ dashboardapp/views/solicitudes.py
@@ -7,37 +7,35 @@ import locale
 
 def inbox(request):
     locale.setlocale(locale.LC_TIME, '')
-    services= []
     if request.session.is_empty():
         domain = request.build_absolute_uri('/')[:-1]
         return redirect(domain+'/digitalapp/ingresar')
-    else:
-        status   = requests.get('http://192.168.10.46:8000/solicitudes/sol/cat_estatus/')
-        statuses = {}
 
+    else:
+
+        response = requests.get('http://192.168.10.46:8000/solicitudes/sol/cat_estatus/')
+        statuses = {}
         requestByStatus = {}
 
-        for state in status.json():
-            statuses.update({ state['key_name']: state['Descripcion']})
-            response = requests.get('http://192.168.10.46:8000/solicitudes/sol/estatus_sol/' + str(state['id']))
-            requestByStatus.update({ state['key_name']: response.json() })
+        StatusArray = [
+            'received',
+            'concluided',
+            'canceled'
+        ]
 
+        for state in response.json():
+
+            if state['key_name'] in StatusArray:
+
+                statuses.update({ state['key_name']: state['estatus_descrip']})
+                responseSol = requests.get('http://192.168.10.46:8000/solicitudes/sol/estatus_sol/' + str(state['id']))
+                requestByStatus.update({ state['key_name']: responseSol.json() })
 
         for key, value in requestByStatus.items():
             for element in value:
-
                 date = datetime.strptime(element['fecha_sol'], "%d/%m/%Y %H:%M")
                 formatedDate = date.strftime("%d %h %Y, %I:%M %p")
                 element.update({'formated_date' : formatedDate})
-
-                archivos = eval(element['archivos'][0])
-                element.update({'archivos': archivos['count']})
-
-                if element['servicios'] != None:
-                    for servicio in element['servicios']:
-                        services.append(eval(servicio))
-
-                    element.update({'servicios': services})
 
         return render(request, "solicitudes/index.html", {
             'statuses': statuses,
