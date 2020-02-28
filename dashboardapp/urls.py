9b838a443904921a5f19b54d31698a0afc481456
Xochitl <xochitl.escamilla@ujed.mx>
2020-02-28 13:30:15 -0600

    Agregar URL para guardar el servicio

--- dashboardapp/urls.py
+++ dashboardapp/urls.py
@@ -7,5 +7,5 @@ urlpatterns = [
     path('solicitudes/bandeja', views.inbox, name='inbox'),
     path('solicitud/<slug:folio>/', views.solicitud, name='solicitud'),
     path('descargar-zip/<slug:folio>/', views.descargarZip, name='download-zip'),
+    path('proceso-servicio/<slug:folio>/', views.guardarServicio, name='proceso-servicio')
 ]
-
