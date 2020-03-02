fdbb49c48b8b3c0154622495349859c65efdb824
Xochitl <xochitl.escamilla@ujed.mx>
2020-03-02 12:24:24 -0600

    Ajustar vista de cards

--- resources/js/admin/components/Services/Cards.vue
+++ resources/js/admin/components/Services/Cards.vue
@@ -1,6 +1,25 @@
 <template>
     <div>
-        <div class="text-center mt-6">
+        <div class="row">
+            <div :class="'db-process-card db-process-card--'+(service.slug)" v-for="service in list">
+                <p class="db-process-card__title"> {{ service.servicio }} </p>
+                <p class="db-process-card__date">  {{ service.fec_subservicio }} </p>
+
+                <span class="badge badge--blue"> {{ service.estatus_name }}  </span>
+
+                <div class="db-process-card__users" >
+                    <div class="user-bar__avatar-container" v-for="user in service.sub_servicioParticipantes">
+                        <img class="user-bar__avatar" src="https://www.pavilionweb.com/wp-content/uploads/2017/03/man-300x300.png" alt="">
+                    </div>
+
+                    <div class="user-bar__avatar-container user-bar__avatar-container--count">
+                        <span class="user-bar__count">+3</span>
+                    </div>
+                </div>
+            </div>
+        </div>
+
+        <div class="text-center mt-6 w-full">
             <button class="btn btn--light w-full" @click="showModal">
                 <span class="icon-plus-circle">
                     <slot name="plus-circle"></slot>
@@ -9,7 +28,6 @@
             </button>
         </div>
 
-        <span v-for="service in addItems()"> {{ service.comentario }} </span>
     </div>
 </template>
 <script>
@@ -21,18 +39,19 @@
                 required: true
             }
         },
+        data() {
+            return {
+                list: this.array,
+            };
+        },
         methods: {
             showModal() {
                 this.$root.$emit('showModal');
             },
             addItems(data) {
                 if(data !== undefined) {
-                    this.array.push(data);
+                    this.list.push(data);
                 }
-
-                console.log(this.array);
-
-                return this.array;
             }
         },
         mounted() {
