8c8a0bccf4d5dfebd34d74042c9ca73fabdb5d96
Xochitl <xochitl.escamilla@ujed.mx>
2020-03-05 14:12:01 -0600

    Agregar componente de los participantes

---
+++ resources/js/admin/components/procesess/Participants.vue
@@ -0,0 +1,27 @@
+<template>
+    <div class="process-modal__participants">
+        <div class="user-bar__avatar-container">
+            <img class="user-bar__avatar" src="https://www.pavilionweb.com/wp-content/uploads/2017/03/man-300x300.png" alt="test" title="test">
+        </div>
+
+        <div class="user-bar__avatar-container">
+            <img class="user-bar__avatar" src="https://www.pavilionweb.com/wp-content/uploads/2017/03/man-300x300.png" alt="test" title="test">
+        </div>
+
+        <div class="user-bar__avatar-container user-bar__avatar-container--add">
+            <span class="user-bar__plus">
+                <slot name="plus"></slot>
+            </span>
+        </div>
+    </div>
+</template>
+
+<script>
+
+    export default {
+
+        mounted() {
+            console.log('participants');
+        },
+    };
+</script>
