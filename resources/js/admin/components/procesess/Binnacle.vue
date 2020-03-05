f82565a68b8d86a666fa0c4adf210c91bddcfc72
Xochitl <xochitl.escamilla@ujed.mx>
2020-03-05 14:10:31 -0600

    Crear componente de bitacora

---
+++ resources/js/admin/components/procesess/Binnacle.vue
@@ -0,0 +1,45 @@
+<template>
+    <div class="container">
+        <div class="media">
+            <div class="media__figure user-bar__avatar-container">
+                <img class="user-bar__avatar" src="https://www.pavilionweb.com/wp-content/uploads/2017/03/man-300x300.png" alt="">
+            </div>
+            <div class="media__body">
+                <p class="mb-0">
+                    <b>Miriam Rivera</b> cambió el estado a <b>En espera</b> y comentó:
+                </p>
+                <blockquote class="blockquote-simple size-sm ml-0 my-2">
+                    <p>Se requiere que nos compartan las fotografías del header para terminar con la interfaz del home.</p>
+                </blockquote>
+                <p class="size-sm color-gray-60">
+                    Lunes 3 Ene 2020, 2:20 p.m.
+                </p>
+            </div>
+        </div>
+
+        <div class="media">
+            <div class="media__figure user-bar__avatar-container">
+                <img class="user-bar__avatar" src="https://www.pavilionweb.com/wp-content/uploads/2017/03/man-300x300.png" alt="">
+            </div>
+            <div class="media__body">
+                <p class="mb-0">
+                    <b>Miriam Rivera</b> cambió el estado a <b>En proceso</b> y comentó:
+                </p>
+                <p class="size-sm color-gray-60">
+                    Lunes 3 Ene 2020, 2:20 p.m.
+                </p>
+            </div>
+        </div>
+    </div>
+</template>
+
+<script>
+
+    export default {
+
+        mounted() {
+            console.log('Binnacle');
+        }
+
+    };
+</script>
