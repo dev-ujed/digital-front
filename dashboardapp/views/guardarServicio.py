0f77d0a0d82edc8945fbf2c99027de4cf08d9fba
Xochitl <xochitl.escamilla@ujed.mx>
2020-03-17 15:13:39 -0600

    Agregar formato a la fecha

--- dashboardapp/views/guardarServicio.py
+++ dashboardapp/views/guardarServicio.py
@@ -2,6 +2,7 @@ from django.shortcuts import render, redirect
 from django.http import HttpResponse, JsonResponse
 import requests
 import json
+from datetime import datetime
 
 def guardarServicio(request, folio):
 
@@ -18,7 +19,16 @@ def guardarServicio(request, folio):
     response = requests.post('http://192.168.10.46:8000/solicitudes/sol/guardar-subservicio/', data=data)
 
     if response.status_code == 201:
-        return JsonResponse(response.json())
+
+        process = response.json()
+
+        #Regresar toda la info del servicio
+
+        date = datetime.strptime(process['fec_subservicio'], "%d/%m/%Y %H:%M")
+        fec_subservicio = date.strftime("%A %d %h %Y, %I:%M %p")
+        process.update({'fec_subservicio' : fec_subservicio.capitalize()})
+
+        return JsonResponse(process)
     else:
         errors.update(response.json())
         return JsonResponse({'errors':errors}, status = 422)
