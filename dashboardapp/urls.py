79b5741890f8a2d7f6ccd3d2b47b526e34acc059
Arturo Morales <arturo.morales@ujed.mx>
2020-03-24 13:44:28 -0600

    Agregar correción para poder guardar archivos de solicitudes.

--- dashboardapp/urls.py
+++ dashboardapp/urls.py
@@ -8,6 +8,7 @@ urlpatterns = [
     path('solicitudes/bandeja', views.inbox, name='inbox'),
     path('solicitud/<slug:folio>/', views.solicitud, name='solicitud'),
     path('descargar-zip/<slug:folio>/', views.descargarZip, name='download-zip'),
+    path('descargararchivo/<path:file>/', views.downloadfile, name='descargararchivo'),
     path('proceso-servicio/<slug:folio>/', views.guardarServicio, name='proceso-servicio'),
     path('solicitudes/proceso', views.solicitudesProceso, name='request-in-process'),
     path('actualizar-titulo/<slug:folio>/', views.actualizarTitulo, name='actualizar-titulo'),
