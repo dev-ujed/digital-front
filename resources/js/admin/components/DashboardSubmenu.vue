46d66580f494ecf17042e6b587b6c6bd1090d79e
Arturo Morales <arturo.morales@ujed.mx>
2020-03-25 14:27:53 -0600

    Ajustar iconos de dashboard.

--- resources/js/admin/components/DashboardSubmenu.vue
+++ resources/js/admin/components/DashboardSubmenu.vue
@@ -8,7 +8,7 @@
                 }"
                 @click="toggle"
             >
-                <img class="dashboard-menu__links-icon" :src="$root.path + '/storage/dashboard/icons/' + submenu.icon" alt="">
+                <img class="dashboard-menu__links-icon" :src="$root.path + '/static/img/icons/dashboard/icons/' + submenu.icon" alt="">
                 {{ submenu.name }}
                 <span class="dashboard-menu__links-arrow" aria-hidden="true"></span>
             </p>
