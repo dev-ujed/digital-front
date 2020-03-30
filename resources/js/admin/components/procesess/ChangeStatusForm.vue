d1c5c4b604cdb86e0d88a63bbad2c3f830fb0e14
Xochitl Escamilla <xochissea@gmail.com>
2020-03-30 11:35:00 -0600

    Emitir el proceso y cambiar detalles en UI

--- resources/js/admin/components/procesess/ChangeStatusForm.vue
+++ resources/js/admin/components/procesess/ChangeStatusForm.vue
@@ -23,7 +23,7 @@
             <div class="process-comment" v-if="status">
                 <div class="form-control w-full mr-2">
                     <label for="comentario">
-                        ¿Deseas incluir un comentario en el correo de notificación al usuario?
+                        Comentario
                         <small class="color-gray-60">Opcional</small>
                     </label>
                     <text-field
@@ -33,7 +33,7 @@
                         v-model="fields.comentario"
                         v-on:keyup.enter="submit">
                     </text-field>
-                    <small class="color-gray-60">Máximo 400 caracteres.</small>
+                    <small class="color-gray-60">Máximo 150 caracteres.</small>
                 <field-errors name="comentario"></field-errors>
             </div>
 
@@ -87,6 +87,7 @@
 
                     this.$root.$emit('closeDetailServiceModal');
                     this.$root.$emit('showEmailModal');
+                    this.$root.$emit('currentProcess', response.data);
 
                     this.isFinished = true;
                 }
